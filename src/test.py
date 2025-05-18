841924eb5cd8fde3e65c05d6d8fa7b537c4a50c3
Marcel Peplies <marcel.peplies@uni-oldenburg.de>
2025-05-18 10:33:56 +0200

    Remove files from docker setup

--- src/test.py
+++
@@ -1,9 +0,0 @@
-from docker_utils import start_overpass_server
-import osmnx as ox
-
-# Lokalen Server starten
-start_overpass_server()
-
-# Overpass-URL auf lokalen Server umstellen
-ox.settings.overpass_endpoint = "http://localhost:12345/api/interpreter"
-ox.settings.overpass_rate_limit = False
