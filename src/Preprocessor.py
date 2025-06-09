e85f3b2185b2de6fc617ae038f39a6ec9310ebb2
Marcel Peplies <marcel.peplies@uni-oldenburg.de>
2025-06-09 13:08:12 +0200

    Add delete to the preprocessor of the input file for the spilt, if the input file is not the raw file

--- src/Preprocessor.py
+++ src/Preprocessor.py
@@ -1,9 +1,9 @@
+import datetime
 import math
 import os
 import subprocess
-
-from src.main import bounding_box
-
+import time
+import traceback
 
 class Preprocessor:
 
@@ -28,10 +28,10 @@ class Preprocessor:
         new_file_name_parameter = f'-o="{new_file_name}"'
         bounding_box_parameter = f'-b="{min_lat}, {min_lon}, {max_lat}, {max_lon}"'
 
-        print("Command: " + os.path.join(f'.\\{self.path_to_osmcovert}', path_to_raw_file, bounding_box_parameter, new_file_name_parameter))
-        test = subprocess.run('.\\resources\\osmconvert64-0.8.8p.exe resources\\raw\\africa-latest.osm.pbf -b="-73.0774531, -25.5731173, -32.03726016666666, 7.335998366666665" -o="resources\\buffer\\africa-latest_-73.0774531_-25.5731173_-32.03726016666666_7.335998366666665.osm.pbf"')
-        print("Returncode:")
-        print(test.returncode)
+        print_to_console("Command: " + os.path.join(f'.\\{self.path_to_osmcovert}', path_to_raw_file, bounding_box_parameter, new_file_name_parameter))
+        test = subprocess.run(f'.\\resources\\osmconvert64-0.8.8p.exe resources\\raw\\africa-latest.osm.pbf -b="{min_lat}, {min_lon}, {max_lat}, {max_lon}" -o="resources\\buffer\\africa-latest_{min_lat}_{min_lon}_{max_lat}_{max_lon}.osm.pbf"')
+        print_to_console("Returncode:")
+        print_to_console(str(test.returncode))
 
         return new_file_name
 
@@ -42,7 +42,7 @@ class Preprocessor:
 
         return raw_statistic
 
-    def split_file(self, file_size_gb:float, statistics_dict:dict, raw_file:str):
+    def split_file(self, file_size_gb:float, statistics_dict:dict, raw_file_path:str, raw_file=False):
 
         # Files larger than 2GB are cut into small pieces
         # Based on the file size in GB, the file is split into theoretical 1 GB files
@@ -56,97 +56,102 @@ class Preprocessor:
         latitude_min = float(statistics_dict['lat min'])
         latitude_max = float(statistics_dict['lat max'])
 
-        print("Split size: " + str(split_size))
-        print("Lon min: " + str(longitudinal_min))
-        print("Lon max: " + str(longitudinal_max))
-        print("Lat min: " + str(latitude_min))
-        print("Lat min: " + str(latitude_max))
+        print_to_console("Split size: " + str(split_size))
 
         longitudinal_diff = abs(longitudinal_min - longitudinal_max)
         latitude_diff = abs(latitude_min - latitude_max)
 
-        print("longitudinal_diff: " + str(longitudinal_diff))
-        print("latitude_diff: " + str(latitude_diff))
-
         offset = 0.00001
         longitudinal_split = longitudinal_diff / split_size
         latitude_split = latitude_diff / split_size
-
-        print("longitudinal_split: " + str(longitudinal_split))
-        print("latitude_split: " + str(latitude_split))
 
         lon_min_bound = -180.0
         lon_max_bound = 180.0
         lat_min_bound = -90.0
         lat_max_bound = 90.0
 
-        for i in range(split_size):
+        path_to_new_file = ''
 
-            print("Range: " + str(i))
+        # Longitudinal
+        for x in range(split_size):
 
-            new_lon_min = longitudinal_min + (i * longitudinal_split) - offset
-            new_lon_max = longitudinal_min + ((i + 1) * longitudinal_split) + offset
-            new_lat_min = latitude_min + (i * latitude_split) - offset
-            new_lat_max = latitude_min + ((i + 1) * latitude_split) + offset
+            new_lon_min = longitudinal_min + (x * longitudinal_split) - offset
+            new_lon_max = longitudinal_min + ((x + 1) * longitudinal_split) + offset
 
             if new_lon_min < lon_min_bound:
                 new_lon_min = lon_min_bound
             if new_lon_max > lon_max_bound:
                 new_lon_max = lon_max_bound
-            if new_lat_min < lat_min_bound:
-                new_lat_min = lat_min_bound
-            if new_lat_max > lat_max_bound:
-                new_lat_max = lat_max_bound
 
-            print("New values:")
-            print("new_lon_min: " + str(new_lon_min))
-            print("new_lon_max: " + str(new_lon_max))
-            print("new_lat_min: " + str(new_lat_min))
-            print("new_lat_max: " + str(new_lat_max))
+            # Latitude
+            for y in range(split_size):
 
-            # Create sub-files
-            path_to_new_file = self.create_sub_file(os.path.join(self.path_to_raw, raw_file), new_lat_min, new_lon_min, new_lat_max, new_lon_max)
-            print(f'New file: {path_to_new_file}')
+                new_lat_min = latitude_min + (y * latitude_split) - offset
+                new_lat_max = latitude_min + ((y + 1) * latitude_split) + offset
 
-            # Get file size
-            file_size = os.path.getsize(path_to_new_file)
-            file_size_gb = file_size / math.pow(10, 9)
+                print_to_console(f'Range x: {x}/{split_size} | Range y: {y}/{split_size}')
 
-            # Get file statistics
-            new_file_statistics = self.get_osm_statistics(path_to_new_file)
+                if new_lat_min < lat_min_bound:
+                    new_lat_min = lat_min_bound
+                if new_lat_max > lat_max_bound:
+                    new_lat_max = lat_max_bound
 
-            if file_size_gb > 2.0:
+                print_to_console("New values:")
+                print_to_console("new_lon_min: " + str(new_lon_min))
+                print_to_console("new_lon_max: " + str(new_lon_max))
+                print_to_console("new_lat_min: " + str(new_lat_min))
+                print_to_console("new_lat_max: " + str(new_lat_max))
 
-                print("File is larger than 2 GB!" + str(file_size_gb))
+                # Create sub-files
+                path_to_new_file = self.create_sub_file(os.path.join(self.path_to_raw, raw_file_path), new_lat_min, new_lon_min, new_lat_max, new_lon_max)
+                print_to_console(f'New file: {path_to_new_file} from {raw_file_path}')
 
-                # Create statistics dict
-                new_statistics_dict = {}
-                for statistic in new_file_statistics.split("\n"):
+                # Get file size
+                file_size = os.path.getsize(path_to_new_file)
+                file_size_gb = file_size / math.pow(10, 9)
 
-                    if statistic != '':
-                        split = statistic.split(":", 1)
-                        new_statistics_dict[split[0]] = split[1]
+                # Get file statistics
+                new_file_statistics = self.get_osm_statistics(path_to_new_file)
 
-                self.split_file(file_size_gb, new_statistics_dict, raw_file)
+                if file_size_gb > 2.0:
 
-            else:
-                print("File is smaller than 2 GB!" + str(file_size_gb))
+                    print_to_console("File is larger than 2 GB!" + str(file_size_gb))
 
-                self.preprocessed_files_statistics[os.path.basename(path_to_new_file).split('/')[-1]] = new_file_statistics
+                    # Create statistics dict
+                    new_statistics_dict = {}
+                    for statistic in new_file_statistics.split("\n"):
 
+                        if statistic != '':
+                            split = statistic.split(":", 1)
+                            new_statistics_dict[split[0]] = split[1]
 
+                    self.split_file(file_size_gb, new_statistics_dict, path_to_new_file)
+
+                else:
+                    print_to_console("File is smaller than 2 GB!" + str(file_size_gb))
+
+                    self.preprocessed_files_statistics[os.path.basename(path_to_new_file).split('/')[-1]] = new_file_statistics
+
+        # Delete original file
+        if not raw_file:
+            try:
+                os.remove(path_to_new_file)
+            except IOError:
+                print_to_console(f'Error while trying to remove file: {traceback.format_exc()}')
+            finally:
+                print_to_console(f"File '{path_to_new_file}' deleted successfully.")
 
 
     def main(self):
 
         for raw_file in os.listdir(self.path_to_raw):
 
-            print(f'Processing {raw_file}')
+            print_to_console(f'Processing Raw-File: {raw_file}')
 
             # Get file size
             file_size = os.path.getsize(os.path.join(self.path_to_raw, raw_file))
             file_size_gb = file_size / math.pow(10, 9)
-            print(f'Size in GB: {file_size_gb}')
+            print_to_console(f'Size in GB: {file_size_gb}')
 
             # Get file statistics
             file_statistics = self.get_osm_statistics(os.path.join(self.path_to_raw, raw_file))
@@ -165,10 +170,17 @@ class Preprocessor:
             # Each file should be smaller than 2GB
             if file_size_gb > 2.0:
 
-                print("File is larger than 2 GB!")
+                print_to_console("File is larger than 2 GB!")
 
-                self.split_file(file_size_gb, statistics_dict, raw_file)
+                self.split_file(file_size_gb, statistics_dict, raw_file, raw_file=True)
 
+
+def print_to_console(message:str):
+
+    current_datetime = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+    print(f'{current_datetime}: {message}')
+    
 
 if __name__ == '__main__':
 
