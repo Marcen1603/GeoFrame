92b57e293a592ed66878e51e31c882dfd9542a25
Marcel Peplies <marcel.peplies@uni-oldenburg.de>
2025-06-09 13:07:44 +0200

    Add test file for the preprocessor

---
+++ test/TestPreprocessor.py
@@ -0,0 +1,22 @@
+import unittest
+
+class TestPreprocessor(unittest.TestCase):
+
+    def test_raw_files(self):
+        for x in range(5):
+            print(x)
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
