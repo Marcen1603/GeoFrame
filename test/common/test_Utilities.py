0f3fe739cf970431d4cd0e08beaab14180d85555
Marcel Peplies <mpeplies03@gmail.com>
2025-10-04 13:33:32 +0200

    Changes for also local execution

--- test/common/test_Utilities.py
+++ test/common/test_Utilities.py
@@ -1,14 +1,24 @@
 # tests/test_utils.py
 import os
 import re
+import platform
 
 from src.common.Utilities import print_to_console, extract_osm_statistics
 
-path_to_osm_convert_linux = os.path.join(
-    'src', 'resources', 'osmconvert', 'osmconvert')
-osm_test_file_path = os.path.join(
-    'test', 'resources', 'planet-latest_89.99999_-18.00001_108.00001_-8.99999.osm.pbf')
 
+path_to_osm_convert_windows = os.path.join(
+    '..', '..', 'src', 'resources', 'osmconvert', 'osmconvert64-0.8.8p.exe')
+path_to_osm_convert_linux = os.path.join(
+    '..', '..', 'src', 'resources', 'osmconvert', 'osmconvert')
+osm_test_file_path = os.path.join('..', 'resources', 'planet-latest_89.99999_-18.00001_108.00001_-8.99999.osm.pbf')
+
+osm_convert_path = None
+if platform.system().lower() == 'linux':
+    osm_convert_path = path_to_osm_convert_linux
+elif platform.system().lower() == 'windows':
+    osm_convert_path = path_to_osm_convert_windows
+else:
+    raise NotImplementedError(f'Not supported for the os: {platform.system().lower()}')
 
 def test_print_to_console(capsys):
 
@@ -25,7 +35,7 @@ def test_print_to_console(capsys):
 
 def test_extract_osm_statistics():
 
-    osm_statistics = extract_osm_statistics(path_to_osm_convert_linux, osm_test_file_path)
+    osm_statistics = extract_osm_statistics(osm_convert_path, osm_test_file_path)
     assert 'lon min' in osm_statistics
     assert 'lon max' in osm_statistics
     assert 'lat min' in osm_statistics
