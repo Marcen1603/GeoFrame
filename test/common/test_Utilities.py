c39b06c4981201c3c486aef0e3f0622bdb2801f8
Marcel Peplies <mpeplies03@gmail.com>
2025-10-04 10:43:57 +0200

    Add test for extract_osm_statistics and paths for testing

--- test/common/test_Utilities.py
+++ test/common/test_Utilities.py
@@ -1,10 +1,16 @@
 # tests/test_utils.py
+import os
 import re
 
-from src.common.Utilities import print_to_console
+from src.common.Utilities import print_to_console, extract_osm_statistics
+
+path_to_osm_convert_linux = os.path.join(
+    'src', 'resources', 'osmconvert', 'osmconvert')
+osm_test_file_path = os.path.join(
+    'test', 'resources', 'south-america-latest.osm.pbf')
 
 
-def test_print_to_console_output(capsys):
+def test_print_to_console(capsys):
 
     print_to_console("Hello World")
 
@@ -15,3 +21,10 @@ def test_print_to_console_output(capsys):
     assert re.match(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}", output)
     assert "Hello World" in output
     assert "(Thread:" in output
+
+
+def test_extract_osm_statistics():
+
+    osm_statistics = extract_osm_statistics(path_to_osm_convert_linux, osm_test_file_path)
+
+    assert osm_statistics is not None
